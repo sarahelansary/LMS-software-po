eb39fb80d3986f9ba1a0e44cfac480e014d634c8
zainab445 <134068910+zainab445@users.noreply.github.com>
2024-12-23 15:54:40 +0200

    Add files via upload

--- RestApiWithOutDb/src/main/java/com/RestApiWithOutDb/RestApiWithOutDb/model/StudentQuizScore.java
+++ RestApiWithOutDb/src/main/java/com/RestApiWithOutDb/RestApiWithOutDb/model/StudentQuizScore.java
@@ -3,10 +3,14 @@ package com.RestApiWithOutDb.RestApiWithOutDb.model;
 public class StudentQuizScore {
     private Users student;
     private Integer score;
+    private String feedback;
+    
 
-    // Getters and setters
+    
     public Users getStudent() { return student; }
     public void setStudent(Users student) { this.student = student; }
     public Integer getScore() { return score; }
     public void setScore(Integer score) { this.score = score; }
+    public String getfeedback(){return this.feedback = feedback;}
+    public void setFeedback(String feedback){;}
 }
